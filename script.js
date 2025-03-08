6371063a034465aa8491007e7b2e65d739c58f33
eshakar <102459513+eshakar@users.noreply.github.com>
2025-03-08 21:14:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstWord(s) {
   // your code here
+	if (s.length === 0) {
+		return s;
+	} else {
+		let spaceIndex = s.indexOf(' ');
+
+		
+
+		if (spaceIndex === -1) {
+			return s;
+		} else {
+			return s.substring(0, spaceIndex);
+		}
+	}
 }
 
 // Do not change the code below
