d9981c22409c72b49853a0a6cc24d0156defd72f
eshakar <102459513+eshakar@users.noreply.github.com>
2025-03-08 21:26:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 function firstWord(s) {
   // your code here
-	if (s.length === 0) {
-		return s;
-	} else {
-		let spaceIndex = s.indexOf(' ');
+	
+	s = s.trim();
 
-		
-
-		if (spaceIndex === -1) {
-			return s;
-		} else {
-			return s.substring(0, spaceIndex);
-		}
-	}
+  if (s.length === 0) {
+    return s;
+  } else {
+    let spaceIndex = s.indexOf(' ');  
+    if (spaceIndex === -1) {
+      return s;
+    } else {
+      return s.substring(0, spaceIndex);
+    }
+  }
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
